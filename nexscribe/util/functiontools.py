67d79e7384d6873ea7e1492c9068ca7719835969
pixelsapphire <35204889+pixelsapphire@users.noreply.github.com>
2026-04-06 18:35:17 +0200

    Add functiontools.nullsafe

--- nexscribe/util/functiontools.py
+++ nexscribe/util/functiontools.py
@@ -1,7 +1,11 @@
 from typing import Iterable
 
-from nexscribe.util.types import Consumer
+from nexscribe.util.types import Consumer, Function
 
 
 def foreach[T](function: Consumer[T], iterable: Iterable[T]) -> None:
     for item in iterable: function(item)
+
+
+def nullsafe[T, R](function: Function[T, R], obj: T | None) -> R | None:
+    return function(obj) if obj is not None else None
